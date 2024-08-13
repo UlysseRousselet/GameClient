0d4fae9e23606fa8e08dbe65ba9b226862d6edde
Ulysse <ulysse.rousselet@epitech.eu>
2024-08-13 16:30:30 +0200

    displacment change

--- src/graphic/raylib/raylib.cpp
+++ src/graphic/raylib/raylib.cpp
@@ -20,37 +20,41 @@ void Raylib::handleKeyEvent()
 {
     bool asMoved = false;
     if (IsKeyDown(KEY_W)) {
-        UpdateCameraPro(&camera._camera, (Vector3){GetFrameTime() * 15, 0.0f, 0.0f}, (Vector3){0.0f, 0.0f, 0.0f}, 0.0f);
+        // UpdateCameraPro(&camera._camera, (Vector3){GetFrameTime() * 15, 0.0f, 0.0f}, (Vector3){0.0f, 0.0f, 0.0f}, 0.0f);
         gameData->player.posx += GetFrameTime() * cos(gameData->player.rot);
-        gameData->player.posy += GetFrameTime() * sin(gameData->player.rot);
+        gameData->player.posz += GetFrameTime() * sin(gameData->player.rot);
         asMoved = true;
     }
     if (IsKeyDown(KEY_S)) {
-        UpdateCameraPro(&camera._camera, (Vector3){-GetFrameTime() * 15, 0.0f, 0.0f}, (Vector3){0.0f, 0.0f, 0.0f}, 0.0f);
+        gameData->player.posx -= GetFrameTime() * cos(gameData->player.rot);
+        gameData->player.posz -= GetFrameTime() * sin(gameData->player.rot);
         asMoved = true;
     }
     if (IsKeyDown(KEY_A)) {
-        UpdateCameraPro(&camera._camera, (Vector3){0.0f, -GetFrameTime() * 15, 0.0f}, (Vector3){0.0f, 0.0f, 0.0f}, 0.0f);
+        gameData->player.posx += GetFrameTime() * cos(gameData->player.rot - PI / 2);
+        gameData->player.posz += GetFrameTime() * sin(gameData->player.rot - PI / 2);
         asMoved = true;
     }
     if (IsKeyDown(KEY_D)) {
-        UpdateCameraPro(&camera._camera, (Vector3){0.0f, GetFrameTime() * 15, 0.0f}, (Vector3){0.0f, 0.0f, 0.0f}, 0.0f);
+        gameData->player.posx += GetFrameTime() * cos(gameData->player.rot + PI / 2);
+        gameData->player.posz += GetFrameTime() * sin(gameData->player.rot + PI / 2);
         asMoved = true;
     }
+    std::cout << gameData->player.posz << "\n";
     // if (IsKeyDown(KEY_SPACE))
     //     UpdateCameraPro(&camera._camera, (Vector3){0.0f, 0.0f, GetFrameTime() * 15}, (Vector3){0.0f, 0.0f, 0.0f}, 0.0f);
     // if (IsKeyDown(KEY_LEFT_SHIFT))
     //     UpdateCameraPro(&camera._camera, (Vector3){0.0f, 0.0f, -GetFrameTime() * 15}, (Vector3){0.0f, 0.0f, 0.0f}, 0.0f);
     if (IsKeyDown(KEY_LEFT))
-        UpdateCameraPro(&camera._camera, (Vector3){0.0f, 0.0f, 0.0f}, (Vector3){-GetFrameTime() * 30, 0.0f, 0.0f}, 0.0f);
+        gameData->player.rot += 0.1;
     if (IsKeyDown(KEY_RIGHT))
-        UpdateCameraPro(&camera._camera, (Vector3){0.0f, 0.0f, 0.0f}, (Vector3){GetFrameTime() * 30, 0.0f, 0.0f}, 0.0f);
+        gameData->player.rot -= 0.1;
     if (IsKeyDown(KEY_UP))
         UpdateCameraPro(&camera._camera, (Vector3){0.0f, 0.0f, 0.0f}, (Vector3){0.0f, -GetFrameTime() * 30, 0.0f}, 0.0f);
     if (IsKeyDown(KEY_DOWN))
         UpdateCameraPro(&camera._camera, (Vector3){0.0f, 0.0f, 0.0f}, (Vector3){0.0f, GetFrameTime() * 30, 0.0f}, 0.0f);
-    UpdateCameraPro(&camera._camera, (Vector3){0.0f, 0.0f, 0.0f}, (Vector3){0.0f, 0.0f, 0.0f}, -GetMouseWheelMove()*2.0f);
-    UpdateCameraPro(&camera._camera, (Vector3){0.0f, 0.0f, 0.0f}, (Vector3){GetMouseDelta().x*0.05f, GetMouseDelta().y*0.05f, 0.0f}, 0.0f);
+    //UpdateCameraPro(&camera._camera, (Vector3){0.0f, 0.0f, 0.0f}, (Vector3){0.0f, 0.0f, 0.0f}, -GetMouseWheelMove()*2.0f);
+    //UpdateCameraPro(&camera._camera, (Vector3){0.0f, 0.0f, 0.0f}, (Vector3){GetMouseDelta().x*0.05f, GetMouseDelta().y*0.05f, 0.0f}, 0.0f);
     if (asMoved) {
         camera.update();
 
@@ -71,6 +75,8 @@ void Raylib::render()
     EndMode3D();
     DrawText("test", 190, 200, 20, LIGHTGRAY);
     EndDrawing();
-    // std::cout << "GameData posx: " << gameData->player.posx << std::endl;
-    // std::cout << "Camera posx: " << camera._camera.position.x << std::endl;
+    std::cout << "GameData posx: " << gameData->player.posx << std::endl;
+    std::cout << "Camera posx: " << camera._camera.position.x << std::endl;
+    std::cout << "GameData posy: " << gameData->player.posy << std::endl;
+    std::cout << "Camera posy: " << camera._camera.position.y << std::endl;
 }
